425b46fe789e90744a7d1f0a767fedfae63564c2
lurc-zmei <lurc-zmei@inbox.ru>
2025-10-22 17:27:26 +0300

    Add PageController

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 Route::get('/', function () {
     return 'Hello World!';
 })->name('root');
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+Route::get('about', [PageController::class, 'about'])->name('about');
