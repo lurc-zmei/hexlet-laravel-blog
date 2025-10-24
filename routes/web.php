18e4d9d779ffc3758cb705585a5252f9149bd37a
lurc-zmei <lurc-zmei@inbox.ru>
2025-10-24 09:51:00 +0300

    Add article/{id} show

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,11 @@ Route::get('/', function () {
     return 'Welcome';
 })->name('root');
 
-Route::get('about', [PageController::class, 'about'])->name('about');
+Route::get('about', [PageController::class, 'about'])
+    ->name('about');
 
-Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
+Route::get('articles', [ArticleController::class, 'index'])
+    ->name('articles.index');
+
+Route::get('articles/{id}', [ArticleController::class, 'show'])
+    ->name('articles.show');
